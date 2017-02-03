e38273411fc1c5a3de866b18eb36f197ff1355a2
KristiyanVasilev <make_05@abv.bg>
2017-02-03 20:34:02 +0200

    Upload PhonebookUpgrade

---
+++ DictionariesExercises/PhonebookUpgrade/PhonebookUpgrade.cs
@@ -0,0 +1,49 @@
+﻿namespace PhonebookUpgrade
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class PhonebookUpgrade
+    {
+        public static void Main()
+        {       
+            var input = new List<string>();
+            var phonebook = new SortedDictionary<string, string>();
+            do
+            {
+                input = Console.ReadLine().Split(' ').ToList();
+                if (input[0] == "A")
+                {
+                    //phonebook.Add(input[1], input[2]);
+                    phonebook[input[1]] = input[2];
+                }
+                else if (input[0] == "S" && !phonebook.ContainsKey(input[1]))
+                {
+                    Console.WriteLine("Contact {0} does not exist.", input[1]);
+                }
+                else if (input[0] == "S" && phonebook.ContainsKey(input[1]))
+                {
+
+                    foreach (var name in phonebook)
+                    {
+                        if (name.Key == input[1])
+                        {
+                            Console.WriteLine($"{name.Key} -> {name.Value}");
+                        }                        
+                    }
+                }
+                else if (input[0] == "ListAll")
+                {
+                    foreach (var name in phonebook)
+                    {
+                        Console.WriteLine($"{name.Key} -> {name.Value}");
+                    }
+                }
+
+            } while (input[0] != "END");
+
+        }
+
+    }
+}

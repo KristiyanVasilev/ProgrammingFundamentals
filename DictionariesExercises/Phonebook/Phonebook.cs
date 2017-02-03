c50691b0db18668ae9304878f6a09a539b40daa3
KristiyanVasilev <make_05@abv.bg>
2017-02-03 20:09:12 +0200

    Uploading Phonebook project

---
+++ DictionariesExercises/Phonebook/Phonebook.cs
@@ -0,0 +1,41 @@
+﻿namespace Phonebook
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Phonebook
+    {
+        public static void Main()
+        {
+            var input = new List<string>();
+            var phonebook = new Dictionary<string, string>();
+            do
+            {
+                input = Console.ReadLine().Split(' ').ToList();
+                if (input[0] == "A")
+                {
+                    //phonebook.Add(input[1], input[2]);
+                    phonebook[input[1]] = input[2];
+                }
+                else if (input[0] == "S" && !phonebook.ContainsKey(input[1]))
+                {
+                    Console.WriteLine("Contact {0} does not exist.", input[1]);
+                }
+                else if (input[0] == "S" && phonebook.ContainsKey(input[1]))
+                {
+
+                    foreach (var name in phonebook)
+                    {
+                        if (name.Key == input[1])
+                        {
+                            Console.WriteLine($"{name.Key} -> {name.Value}");
+                        }
+                    }
+                }
+            } while (input[0] != "END");
+
+
+        }
+    }
+}

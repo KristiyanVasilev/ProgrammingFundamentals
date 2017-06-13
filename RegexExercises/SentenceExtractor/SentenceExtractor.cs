01e2625233abd587f19689c6f2d092ae35d333a5
KristiyanVasilev <make_05@abv.bg>
2017-06-13 22:27:58 +0300

    Added Sentence Extractor

---
+++ RegexExercises/SentenceExtractor/SentenceExtractor.cs
@@ -0,0 +1,20 @@
+﻿namespace SentenceExtractor
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    public class SentenceExtractor
+    {
+        public static void Main()
+        {
+            var word = Console.ReadLine();
+            var text = Console.ReadLine();
+            var pattern = $@"([a-zA-Z0-9 ]+\b{word}\b\s*.*?[?!.])";
+            var matches = Regex.Matches(text, pattern);
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match.Groups[0].Value);
+            }
+        }
+    }
+}
